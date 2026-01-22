92c840e91ef415629fb020beb50f990fd5f4449c
Nguyen Van Quy <142620727+nvqq10102005@users.noreply.github.com>
2026-01-22 09:43:56 +0700

    docs: ...

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,5 @@
 import axios from "axios"
 
 export const getProductsApi = async () => {
-    try {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/products`);
-    } catch (error) {
-        
-    }
+    await axios.get(`${import.meta.env.VITE_API_URL}/products`);
 }
